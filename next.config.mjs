d052414cdc06b66926d0835c186d193b2d037fee
Solitarius <waynevip2526@gmail.com>
2024-02-14 12:56:03 +0900

    ベースパス修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // ↓ next build を実行する際に Static Export を利用します。
-    output: 'export'
+    output: 'export',
+    basePath: "/<SekaiKura.github.io>",
+    images: {
+      unoptimized: true,
+    },
 };
 
 export default nextConfig;
