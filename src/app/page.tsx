abdee91bc3d378117e253b2e00a6dd99e36a8b03
Solitarius <waynevip2526@gmail.com>
2024-02-19 09:01:21 +0900

    ファイル分割、カテゴリー追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,66 +3,84 @@ import Image from 'next/image'
 import Head from 'next/head'
 import styles from '../layout.module.css'
 import { basePath } from '../../next.config'
+import { images } from '../app/imagedate' 
 import { useState, useEffect } from 'react'
 
-// 画像データ
-const images = [
-  { id: 1, url: '1_0.jpg', mainCategory: 'Category1', subCategory: 'SubCategory1' },
-  { id: 2, url: '1_1.jpg', mainCategory: 'Category1', subCategory: 'SubCategory2' },
-  { id: 3, url: '1_2.jpg', mainCategory: 'Category2', subCategory: 'SubCategory3' },
-  { id: 4, url: '1_3.jpg', mainCategory: 'Category2', subCategory: 'SubCategory4' },
-  { id: 5, url: '1_4.jpg', mainCategory: 'Category3', subCategory: 'SubCategory2' },
-  { id: 5, url: '1_5.jpg', mainCategory: 'Category3', subCategory: 'SubCategory1' },
-];
+
+// const images = [
+//   { id: 1, url: '1_0.jpg', unit: 'Unit1', characterName: 'Character1', rarity: 'Rare' },
+//   { id: 2, url: '1_1.jpg', unit: 'Unit2', characterName: 'Character2', rarity: 'Rare' },
+//   { id: 3, url: '1_2.jpg', unit: 'Unit3', characterName: 'Character3', rarity: 'Rare' },
+//   { id: 4, url: '1_3.jpg', unit: 'Unit4', characterName: 'Character4', rarity: 'Rare' },
+//   { id: 5, url: '1_4.jpg', unit: 'Unit5', characterName: 'Character2', rarity: 'Rare' },
+//   { id: 5, url: '1_5.jpg', unit: 'Unit2', characterName: 'Character3', rarity: 'Rare' },
+// ];
 
 export default function Layout({}) {
-  //const images = ['1_0.jpg', '1_1.jpg', '1_2.jpg', '1_3.jpg', '1_4.jpg', '1_5.jpg', '1_6.jpg', '1_7.jpg', '1_8.jpg', '1_9.jpg', '1_10.jpg', '1_11.jpg', '1_12.jpg', '1_13.jpg'] 
   const BASE_PATH = basePath ? basePath : ""
 
-  const [selectedMainCategory, setSelectedMainCategory] = useState('');
-  const [selectedSubCategory, setSelectedSubCategory] = useState('');
-  
-  const filteredImages = images.filter(image =>
-    (selectedMainCategory === '' || image.mainCategory === selectedMainCategory) &&
-    (selectedSubCategory === '' || image.subCategory === selectedSubCategory)
-  );
+  const [selectedUnit, setSelectedUnit] = useState('');
+  const [selectedCharacterName, setSelectedCharacterName] = useState('');
+  const [selectedRarity, setSelectedRarity] = useState('');
+
+  const filteredImages = images.filter((image) => {
+    return (
+      (selectedUnit === '' || image.unit === selectedUnit) &&
+      (selectedCharacterName === '' || image.characterName === selectedCharacterName) &&
+      (selectedRarity === '' || image.rarity === selectedRarity)
+    );
+  });
+
+  const units = [...new Set(images.map((image) => image.unit))];
+  const characterNames = selectedUnit
+  ? [...new Set(images.filter((image) => image.unit === selectedUnit).map((image) => image.characterName))]
+  : [];
+const rarities = ['Common', 'Rare', 'Epic', 'Legendary'];
 
   return (
     <div className={styles.container}>
-      <Head>
-        <title>画像ギャラリーサイト</title>
-      </Head>
+      <h1>世界のギャラリー</h1>
 
       <header className={styles.header}>
-        <select value={selectedMainCategory} onChange={e => setSelectedMainCategory(e.target.value)}>
-          <option value="">すべてのメインカテゴリ</option>
-          <option value="Category1">Category1</option>
-          <option value="Category2">Category2</option>
-          <option value="Category3">Category3</option>
-        </select>
+      <select value={selectedUnit} onChange={(e) => setSelectedUnit(e.target.value)}>
+        <option value="">ユニットを選択</option>
+        {units.map((unit) => (
+          <option key={unit} value={unit}>
+            {unit}
+          </option>
+        ))}
+      </select>
 
-        <select value={selectedSubCategory} onChange={e => setSelectedSubCategory(e.target.value)}>
-          <option value="">すべてのサブカテゴリ</option>
-          <option value="SubCategory1">SubCategory1</option>
-          <option value="SubCategory2">SubCategory2</option>
-          <option value="SubCategory3">SubCategory3</option>
-          <option value="SubCategory4">SubCategory4</option>
-        </select>
+      <select value={selectedCharacterName} onChange={(e) => setSelectedCharacterName(e.target.value)}>
+        <option value="">キャラクター名を選択</option>
+        {characterNames.map((characterName) => (
+          <option key={characterName} value={characterName}>
+            {characterName}
+          </option>
+        ))}
+      </select>
 
-        {/* <div>
-          {filteredImages.map(image => (
-            <img key={image.id} src={image.url} alt="" />
-          ))}
-        </div> */}
+      <select value={selectedRarity} onChange={(e) => setSelectedRarity(e.target.value)}>
+        <option value="">レア度を選択</option>
+        {rarities.map((rarity) => (
+          <option key={rarity} value={rarity}>
+            {rarity}
+          </option>
+        ))}
+      </select>
+
       </header>
 
       <main className={styles.main}>
-        {filteredImages.map((image, index) => (
-          <div key={index}>
-            <Image src={`${BASE_PATH}/images/${image.url}`} alt={`Image ${index}`} width={400} height={250} />
-            <a href={`${BASE_PATH}/images/${image.url}`} download={`Image${index}.jpg`}>Download</a>
+        <div>
+          {filteredImages.map((image) => (
+          <div key={image.id}>
+          {/* 画像表示部分 */}
+          <img src={`${BASE_PATH}/images/${image.url}`} alt={image.characterName} width={400} height={250}/>
+          <p>{image.characterName}</p>
+          </div>
+          ))}
         </div>
-        ))}
       </main>
 
       <footer className={styles.footer}>
