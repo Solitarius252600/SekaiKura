035b717daa6f878e56929277012892a137c52ace
Solitarius <waynevip2526@gmail.com>
2024-04-24 00:29:45 +0900

    ページ修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,12 @@ import { useState, useEffect } from 'react'
 
 // 画像データ
 const images = [
-  { id: 0, url: '1_13.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
-  { id: 7, url: '1_1.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
-  { id: 6, url: '0_0.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
-//  { id: 1, url: 'PJSK_Cardillustration/0_VirtualSinger/0_初音ミク/0_☆4【みんなにお披露目】初音ミク_1.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
-//  { id: 2, url: 'PJSK_Cardillustration/0_VirtualSinger/0_初音ミク/1_☆3【見守るまなざし】初音ミク_0.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
-//  { id: 3, url: 'PJSK_Cardillustration/0_VirtualSinger/0_初音ミク/1_☆3【見守るまなざし】初音ミク_1.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
-//  { id: 4, url: 'PJSK_Cardillustration/0_VirtualSinger/0_初音ミク/2_☆4【絶たれた糸】初音ミク_0.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
-//  { id: 5, url: 'PJSK_Cardillustration/0_VirtualSinger/0_初音ミク/2_☆4【絶たれた糸】初音ミク_1.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
+  { id: 0, url: 'PJSK_Cardillustration/0_VirtualSinger/0_初音ミク/0_☆4【みんなにお披露目】初音ミク_0.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
+  { id: 1, url: 'PJSK_Cardillustration/0_VirtualSinger/0_初音ミク/0_☆4【みんなにお披露目】初音ミク_1.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
+  { id: 2, url: 'PJSK_Cardillustration/0_VirtualSinger/0_初音ミク/1_☆3【見守るまなざし】初音ミク_0.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
+  { id: 3, url: 'PJSK_Cardillustration/0_VirtualSinger/0_初音ミク/1_☆3【見守るまなざし】初音ミク_1.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
+  { id: 4, url: 'PJSK_Cardillustration/0_VirtualSinger/0_初音ミク/2_☆4【絶たれた糸】初音ミク_0.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
+  { id: 5, url: 'PJSK_Cardillustration/0_VirtualSinger/0_初音ミク/2_☆4【絶たれた糸】初音ミク_1.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
 ];
 
 // カテゴリーとサブカテゴリーのデータ
