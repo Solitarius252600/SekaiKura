e78d419d7e2617e4059825157fd30a4deddcd74b
Solitarius <waynevip2526@gmail.com>
2024-04-25 00:22:45 +0900

    定義分け

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,7 @@ import Head from 'next/head'
 import styles from '../layout.module.css'
 import { basePath } from '../../next.config'
 import { useState, useEffect } from 'react'
-
-// 画像データ
-const images = [
-  { id: 0, url: 'PJSK_Cardillustration/0_VirtualSinger/0_初音ミク/0_☆4【みんなにお披露目】初音ミク_0.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
-  { id: 1, url: 'PJSK_Cardillustration/0_VirtualSinger/0_初音ミク/0_☆4【みんなにお披露目】初音ミク_1.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
-  { id: 2, url: 'PJSK_Cardillustration/0_VirtualSinger/0_初音ミク/1_☆3【見守るまなざし】初音ミク_0.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
-  { id: 3, url: 'PJSK_Cardillustration/0_VirtualSinger/0_初音ミク/1_☆3【見守るまなざし】初音ミク_1.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
-  { id: 4, url: 'PJSK_Cardillustration/0_VirtualSinger/0_初音ミク/2_☆4【絶たれた糸】初音ミク_0.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
-  { id: 5, url: 'PJSK_Cardillustration/0_VirtualSinger/0_初音ミク/2_☆4【絶たれた糸】初音ミク_1.jpg', mainCategory: 'Category0', subCategory: 'SubCategory0' },
-];
+import { images } from '../app/images.js'
 
 type CategoryType = {
   [key: string]: string[];
